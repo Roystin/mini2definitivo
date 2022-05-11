3bbf0ab6bdd52a18a44e083e91bcb419db76a53c
Roystin <75227834+Roystin@users.noreply.github.com>
2022-05-11 15:07:41 -0500

    prueba 2.0
    adad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,18 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
 import { UnheroeComponent } from './unheroe/unheroe.component';
+import {UsuariosComponent} from './usuarios/usuarios.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'heroes', component: HeroesComponent },
   {path: 'about', component: AboutComponent },
+  {path: 'usuarios', component: UsuariosComponent},
 
   {path: 'heroe/:id', component: UnheroeComponent},
   {path: 'buscador/:nombreh', component:SearchComponent},
   {path: '**', pathMatch: 'full', redirectTo: 'home'}
+  
 ];
 
 @NgModule({
